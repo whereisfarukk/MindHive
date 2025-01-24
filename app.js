366f73ce745b8c74ee2662da85640c57f4a20d99
whereisfarukk <whereisfarukk@gmail.com>
2025-01-24 12:27:13 +0600

    feat(pages):ejs setup

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
+const mogan = require("morgan");
 const PORT = process.env.PORT || 8080;
+
+// setup view engine
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+// middleware array
+const middleware = [
+  morgan("dev"),
+  express.static("public"), //exposing the public directory to all folder
+  express.urlencoded({ extended: true }), //  to handle all tyoes incoming form data
+  express.json(), //to handle incoming JSON data
+];
+
+app.use(middleware);
 app.get("/", (req, res) => {
   res.json({
     message: "working",
